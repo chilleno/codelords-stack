9d0941ff061cf9b35fa927bb748d45302ce3c91d
chilleno <127969843+chilleno@users.noreply.github.com>
2025-06-26 10:12:52 -0400

    feat: add session validation

--- packages/templates/auth/app/login/page.tsx
+++ packages/templates/auth/app/login/page.tsx
@@ -1,6 +1,11 @@
 import { LoginForm } from "./components/login-form";
+import { auth } from "@/lib/auth"
+import { redirect } from 'next/navigation';
 
-export default function LoginPage() {
+export default async function LoginPage() {
+    const session = await auth();
+    if (session) return redirect("/")
+
     return (
         <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div className="w-full max-w-sm">
